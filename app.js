475f65f2d7a9bbe6fbde626778bc0223ef305905
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-07 19:48:44 -0400

    Init + PrettyFunction

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ class Tree {
         this.root = root;
     };
 };
+
+function buildTree(arr) {
+    //
+};
+
+// Print Tree to Console
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    };
+
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    };
+
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    };
+};
+
+prettyPrint();
+
+  // node app.js
